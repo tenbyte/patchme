ebcdd2ff63e37179925e54b1cbb5202259b235c6
wiesty <106901362+wiesty@users.noreply.github.com>
2025-08-10 17:39:35 +0200

    color codes

--- examples/debian/activate.sh
+++ examples/debian/activate.sh
@@ -10,8 +10,8 @@ SERVICE_FILE="pm_ingest.service"
 TIMER_FILE="pm_ingest.timer"
 SYSTEMD_DIR="/etc/systemd/system"
 
-CYAN='\033[36m'
-WHITE='\033[0m'
+CYAN='\033[0;36m'
+WHITE='\033[1;37m'
 BOLD='\033[1m'
 RESET='\033[0m'
 RED='\033[0;31m'
